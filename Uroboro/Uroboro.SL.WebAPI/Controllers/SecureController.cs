18de35c995bfd7d186d4f09f0c4925f96f3d1665
lucazaccaro <zaccaroluca@gmail.com>
2021-01-23 21:43:01 +0100

    Added Secure APIs

---
+++ Uroboro/Uroboro.SL.WebAPI/Controllers/SecureController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Uroboro.SL.WebAPI.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SecureController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Secure Get OK");
+        }
+
+        [HttpPost]
+        public IActionResult Post()
+        {
+            return Ok("Secure Post OK");
+        }
+    }
+}
