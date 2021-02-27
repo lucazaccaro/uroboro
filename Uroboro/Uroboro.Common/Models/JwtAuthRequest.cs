e4069fe6e712c470dab76c0264f7045cdb22d4f6
lucazaccaro <zaccaroluca@gmail.com>
2021-02-27 13:33:10 +0100

    Refactored Uroboro.Common project

---
+++ Uroboro/Uroboro.Common/Models/JwtAuthRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Uroboro.Common.Models
+{
+    public class JwtAuthRequest
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
